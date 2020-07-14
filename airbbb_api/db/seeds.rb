0dff9c5c3122032550aa4f06b6ed812fcb483a93
stobrien89 <stobrien@crimson.ua.edu>
2020-07-14 14:47:54 -0700

    seeded db

--- airbbb_api/db/seeds.rb
+++ airbbb_api/db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+airlines = Airline.create([
+  { 
+    name: "Cathay Pacific",
+    image_url: "https://open-flights.s3.amazonaws.com/Cathay-Pacific.png"
+  }, 
+  { 
+    name: "Lufthansa",
+    image_url: "https://open-flights.s3.amazonaws.com/Lufthansa.png"
+  },
+  { 
+    name: "Delta",
+    image_url: "https://open-flights.s3.amazonaws.com/Delta.png" 
+  }, 
+  { 
+    name: "Alaska Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/Alaska-Airlines.png" 
+  }, 
+  { 
+    name: "Air France",
+    image_url: "https://open-flights.s3.amazonaws.com/Air-France.png" 
+  }, 
+  { 
+    name: "Xiamen Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/XiamenAir.png" 
+  }
+])
+
+reviews = Review.create([
+    {
+        title: 'I love this airline',
+        description: 'A little cramped, food was okay',
+        score: 4,
+        airline: airlines.first
+    },
+    {
+        title: 'Terrible',
+        description: 'hot, smelly, lots of kids',
+        score: 2,
+        airline: airlines.first
+    }
+])
