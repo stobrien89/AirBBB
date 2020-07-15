43453a866aed5a68661358977ec29d3029e6e9ee
stobrien89 <stobrien@crimson.ua.edu>
2020-07-15 09:24:01 -0700

    connected to back-end, json data displayed in console

--- airbbb_frontend/src/components/Airlines/Airlines.jsx
+++ airbbb_frontend/src/components/Airlines/Airlines.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Airlines = () => {
+    const [airlines, setAirlines] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/api/v1/airlines.json')
+        .then(res => console.log(res.data))
+        .catch(res => console.log(res.data))
+    }, [airlines.length])
+    
     return <div>Testing the Airlines index view</div>
 }
 
