eb7fe845af5945dcea3a875d8f5e1088fb6a549b
stobrien89 <stobrien@crimson.ua.edu>
2020-07-15 12:06:53 -0700

    added styled components to index and airline cards

--- airbbb_frontend/src/components/Airlines/AirlineCard.jsx
+++ airbbb_frontend/src/components/Airlines/AirlineCard.jsx
@@ -1,17 +1,52 @@
 import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Card = styled.div`
+    text-align: center;
+    border: 1px solid black;
+    background: yellow;
+`
+const AirlineLogo = styled.div`
+    text-align: center;
+    padding-top: 10px;
+    width: 50px;
+    img {
+        height: 50px;
+        width: 50px;
+        border-radius: 100%;
+        border: 1px solid black;
+    }
+`
+const AirlineName = styled.div`
+    padding: 20px 0 10px 0;
+`
+const LinkContainer = styled.div`
+    margin: 30px 0 20px 0;
+    height 50px;
+
+    a {
+        width: 100%;
+        text-decoration: none;
+        color: white;
+        background: black;
+        border-radius: 5px;
+        border: 1px solid black;
+    }
+`
 
 const AirlineCard = (props) => {
     return (
-        <div>
-            <div className="airline-logo">
+        <Card>
+            <AirlineLogo>
                 <img src={props.attributes.image_url} alt={props.attributes.name}/>
-            </div>
-            <div className="airline-name">{props.attributes.name}</div>
+            </AirlineLogo>
+            <AirlineName>{props.attributes.name}</AirlineName>
             <div className="airline-score">{props.attributes.avg_score}</div>
-            <div className="airline-link">
-                <a href={`/airlines/${props.attributes.slug}`}>View Airline</a>
-            </div>
-        </div>
+            <LinkContainer>
+                <Link to={`/airlines/${props.attributes.slug}`}>View Airline</Link>
+            </LinkContainer>
+        </Card>
     )
 }
 
--- airbbb_frontend/src/components/Airlines/Airlines.jsx
+++ airbbb_frontend/src/components/Airlines/Airlines.jsx
@@ -1,6 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import AirlineCard from './AirlineCard';
 import axios from 'axios';
+import styled from 'styled-components';
+
+const AirlineWrapper = styled.div`
+    max-width: 1200px;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+`
+const AirlineHeader = styled.div`
+    padding: 100px 100px 10px 100px;
+
+    h1 {
+        font-size: 3 rem;
+    }
+`
+const AirlineSubHeader = styled.div`
+    font-size: 2rem;
+    font-weight: 400;
+`
+const AirlineContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 20px;
+    width: 100%;
+`
+
 
 const Airlines = () => {
     const [airlines, setAirlines] = useState([]);
@@ -21,15 +47,15 @@ const Airlines = () => {
     })
     
     return (
-        <div className="wrapper">
-            <header>
+        <AirlineWrapper>
+            <AirlineHeader>
                 <h1>AirBBB</h1>
-                <p>Your source for unfiltered, unbiased airline reviews.</p>
-            </header>
-            <div className="container">
-                <ul>{airlineList}</ul>
-            </div>
-        </div>
+                <AirlineSubHeader>Your source for unfiltered, unbiased airline reviews.</AirlineSubHeader>
+            </AirlineHeader>
+            <AirlineContainer>
+                {airlineList}
+            </AirlineContainer>
+        </AirlineWrapper>
     )
 }
 
