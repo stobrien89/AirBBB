10c8d104e6fa41da9d98c8a1edb2401c80b49227
stobrien89 <stobrien@crimson.ua.edu>
2020-07-16 15:10:10 -0700

    updated average score logic in airline model/serializer

--- airbbb_api/app/models/airline.rb
+++ airbbb_api/app/models/airline.rb
@@ -7,7 +7,10 @@ class Airline < ApplicationRecord
         self.slug = name.parameterize
     end
 
-    def average_score
-        reviews.average(:score).round(2).to_f
+    def add_average
+        return 0 unless reviews.size.positive?
+        
+        average = reviews.average(:score).to_f.round(2) * 100
+        update_column(:average_score, avg)
     end
 end
--- airbbb_api/app/serializers/airline_serializer.rb
+++ airbbb_api/app/serializers/airline_serializer.rb
@@ -2,5 +2,9 @@ class AirlineSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :image_url, :slug
 
+  attribute :average_score do |object|
+    (object.average_score.to_f / 100).to_f.round(2)
+  end
+
   has_many :reviews
 end
