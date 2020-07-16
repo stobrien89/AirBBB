ce8bc575a547191b3d4f8ef39b8ee40ddb38177c
stobrien89 <stobrien@crimson.ua.edu>
2020-07-15 18:18:54 -0700

    fixed cors issue

--- airbbb_api/app/controllers/api/v1/reviews_controller.rb
+++ airbbb_api/app/controllers/api/v1/reviews_controller.rb
@@ -3,7 +3,7 @@ module Api
         class ReviewsController < ApplicationController
 
             def create
-                review = Review.new(review_params)
+                review = airline.reviews.new(review_params)
 
                 if review.save
                     render json: ReviewSerializer.new(review).serialized_json
@@ -23,6 +23,10 @@ module Api
             end
 
             private
+
+            def airline
+                @airline ||= Airline.find(params[:airline_id])
+            end
 
             def review_params
                 params.require(:review).permit(:title, :description, :score, :airline_id)
