e1e03a043cc687d70e063e77855c6aa222664d5f
stobrien89 <stobrien@crimson.ua.edu>
2020-07-18 18:17:45 -0700

    updated config and routes

--- airbbb_api/config/application.rb
+++ airbbb_api/config/application.rb
@@ -32,6 +32,7 @@ module AirbbbApi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.middleware.use ActionDispatch::Cookies
     config.api_only = true
   end
 end
